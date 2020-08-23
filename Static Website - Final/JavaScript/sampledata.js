c8ac1ce0c8bdb9102d4b3d80079d6642dc1702d1
Swathi Korrapatti <swathiravi10695@gmail.com>
2020-08-23 14:23:51 -0400

    Static-Website

---
+++ Static Website - Final/JavaScript/sampledata.js
@@ -0,0 +1,11 @@
+let menu = [{ number: "1", name: "Chicken Curry Roti", description: "Chicken roti cooked delicately with Indian spices.", price: "13.70", available: "yes" },
+{ number: "2", name: "Goat Roti (Bone-In)", description: "Goat roti cooked with mixed vegetables and Indian spices.", price: "14.70", available: "yes" },
+{ number: "3", name: "Jerk Chicken Meal", description: "Savoury and tasty jerk chicken.", price: "8.00", available: "no" },
+{ number: "4", name: "Fried Chicken Meal", description: "Enjoy this crispy and tasty golden-fried chicken.", price: "13.50", available: "yes" },
+{ number: "5", name: "Boneless Curry Chicken Meal", description: "Home-style curry chicken.", price: "12.50", available: "yes" }];
+
+let orders = [{ time: "12:00PM", details: "1 Chicken curry roti", total: "13.70", status: "done" },
+{ time: "12:00PM", details: "1 Chicken curry, 2 Goat roti", total: "34", status: "done" },
+{ time: "1:30PM", details: "2 Jerk Chicken Meal", total: "28.30", status: "started" },
+{ time: "2:44PM", details: "1 Fried Chicken Meal", total: "13.5", status: "not started" },
+{ time: "3:10PM", details: "2 Boneless curry chicken meal", total: "25", status: "not started" }]
